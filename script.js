5d829aa404fc40916134690a5915ba50f5defb56
davronweb009 <davronweb07@mail.ru>
2025-01-27 13:56:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let fruit = ['olma','anor','shaftoli','banan'];
+console.log(fruit)
+let fruit1 = alert('Boshlanishida array uzunligi 4');
+let first = confirm('Siz arrayni malumotlarini olib tashlamoqchimisiz')
+if(first === true){
+    let fruit2 = alert('arrayning uzunligi 3');
+    
+       console.log(fruit.pop());
+       console.log(fruit)
+
+    
+}
+else{
+    let fruit3 = alert('arrayning uzunligi 4');
+    console.log(fruit)
+
+}
